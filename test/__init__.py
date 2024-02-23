a1941b84b7bc58dfe186322bf54240e01f4edbca
oliver-s-lee <osl@st-andrews.ac.uk>
2024-02-23 11:57:02 +0000

    Added initial tests for format conversions (with data)

---
+++ test/__init__.py
@@ -0,0 +1,8 @@
+"""Unit tests for openprattle"""
+
+import importlib.resources
+from pathlib import Path
+
+
+# Path to the data directory.
+DATA = Path(__file__).parent / "data"
---
+++ test/test_formats.py
@@ -0,0 +1,47 @@
+"""Test the openprattle library."""
+
+import pytest
+from pathlib import Path
+
+from openprattle.babel import Openbabel_converter, Pybel_converter, Obabel_converter
+from openprattle.babel import formats
+
+from test import DATA
+
+# List of supported converters.
+BACKENDS = [
+    Pybel_converter,
+    Obabel_converter
+]
+
+READ_FORMATS = formats().read()
+WRITE_FORMATS = formats().write()
+
+
+
+@pytest.mark.formats
+@pytest.mark.parametrize("backend", BACKENDS)
+@pytest.mark.parametrize("write_format", WRITE_FORMATS)
+def test_out_formats(backend, write_format, tmp_path):
+    """
+    """
+    converter = backend(input_file_path = Path(DATA, "Benzene.cml"), input_file_type = "cml")
+
+    converter.convert(write_format, Path(tmp_path, Path("Benzene").with_suffix("." + write_format)))
+
+@pytest.mark.formats
+@pytest.mark.parametrize("backend", BACKENDS)
+@pytest.mark.parametrize("read_format", READ_FORMATS)
+def test_in_formats(backend, read_format, tmp_path):
+    """
+    """
+    input_file_path = Path(DATA, Path("Benzene").with_suffix("." + read_format))
+
+    if not input_file_path.exists():
+        pytest.skip("No '{}' file available to test".format(input_file_path))
+
+    converter = backend(input_file_path = input_file_path, input_file_type = read_format)
+
+    converter.convert("cml", Path(tmp_path, "Benzene.cml"))
+
+
